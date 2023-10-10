9174ea4fa062305d05a61fde76267af193085df4
alexbrunoliveira <alexbrunoliveira@gmail.com>
2023-10-10 16:45:47 -0300

    changing the blog theme

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,16 +1,37 @@
 export const dataFake = [
   {
   "id":"1",
-  "title": "NOVO ADÃO NEGRO EM 3D",
-  "description": "O novo plano da DC no cinema é não ter um plano... ",
-  "photoCover":"https://universoheroico.com.br/wp-content/uploads/2021/03/Adao-Negro.jpeg"
+  "title": "John Wick 4 – Baba Yaga",
+  "description": "Subindo ao pódio com a medalha de prata temos o ressurgido Keanu Reeves. Um dos astros mais gente boa de Hollywood, Reeves merecia um novo sucesso para chamar de seu nossa nova fase de sua carreira. Depois ‘Velocidade Máxima’ e ‘Matrix’, chegou em 2014 ao repertório do ator, o assassino profissional da máfia russa John Wick. O sujeito decide se vingar pela morte de seu cachorrinho, dado pela falecida esposa, e depois disso o sangue nunca mais parou de espirrar. Já são quatro filmes. Mas essa não é uma franquia qualquer, porque a cada episódio tudo aumenta, os filmes ficam maiores e melhores, os elogios dos fãs e da crítica também aumentam, assim como a sua bilheteria igualmente. O ápice (até o momento) é esse quarto, que possui nota 7.9 do público e 94% de aprovação dos críticos.",
+  "photoCover":"https://e0.pxfuel.com/wallpapers/430/236/desktop-wallpaper-dope-artwork-john-wick-baba-yaga-and-stock.jpg"
   },
   {
-    "id":"2",
-    "title": "Novo HQ é anunciado",
-    "description": "Trinity, filha da Mulher Maravilha, ganhará seu próprio quadrinho em 2024",
-    "photoCover":"https://static1.cbrimages.com/wordpress/wp-content/uploads/2023/10/untitled-design-8-1.jpg"
-    }
+    "id":"0",
+    "title": "Creed III",
+    "description": "Mesmo sem Sylvester Stallone nas telas para dar o respaldo como Rocky Balboa (o ator serve apenas como produtor aqui), o jovem Michael B. Jordan manda o recado de que ele e sua franquia derivada conseguem caminhar sozinhos com as próprias pernas. Mais do que isso, pois Jordan assume a direção de seu primeiro filme, e faz bonito. Quanto tempo será que a Marvel demora para convidar o agora cineasta a comandar uma de suas produções? ‘Creed’ é outra franquia que não deve perder tempo em sinalizar um quarto filme, levando em conta o resultado desse terceiro, que traz o protagonista enfrentando um amigo de infância, nas formas do perigoso Jonathan Majors. O filme tem nota 6.8 do público e 88% de aprovação dos críticos.",
+    "photoCover":"https://images6.alphacoders.com/130/1304333.jpg"
+    },
+
+    {
+      "id":"2",
+      "title": "Homem-Aranha: Através do Aranhaverso",
+      "description": "Homem-Aranha: Através do Aranhaverso é a segunda animação sobre a história de Miles Morales, o Homem-Aranha negro. A história, lançada cinco anos depois do primeiro filme da trilogia, narra o envolvimento de Morales dentro do multiverso de Homens-Aranha — e as consequências de seus atos em cada um dos universos existentes. O filme tem um total de 2 horas e 16 minutos e arrancou notas altas da crítica cinematográfica: recebeu 9,1/10 no IMDb e 96% de aprovação no Rotten Tomatoes.  Homem-Aranha: Através do Aranhaverso vale cada segundo de sua duração, até mesmo os créditos iniciais. O filme mantém (e amplia) a excelência de seu antecessor, usa a criatividade como ponto de partida e o próprio estilo de animação como narrativa.",
+      "photoCover":"https://cdn.ome.lt/pUowdbSCipy870Trfy7SymfE9UE=/1200x630/smart/extras/conteudos/homem-aranha-atraves-do-aranhaverso-reacoes.jpg"
+      },
+
+      {
+        "id":"3",
+        "title": "Oppenheimer",
+        "description": "Oppenheimer, mais novo filme de Christopher Nolan, fez um tremendo sucesso em julho deste ano. O longa-metragem, 12º da carreira de Nolan, conta a história do 'pai' da primeira bomba atômica, J. Robert Oppenheimer, produzida pelo Projeto Manhattan, no Teste Trinity, e detonada no Novo México em 1945, meses antes do ataque à Hiroshima e Nagazaki — que deixou um total de mais de 110 mil pessoas mortas nas duas cidades.Baseado na biografia <strong>American Prometheus: The Triumph and Tragedy of J. Robert Oppenheimer (Oppenheimer: o triunfo e a tragédia do Prometheu americano)</strong>, de Kai Bird e Martin Sherwin, publicada em 2006 e vencedora do Prêmio Pulitzer, o filme não só traz uma das maiores problemáticas do envolvimento dos Estados Unidos na Segunda Guerra Mundial, como também a explora pela atuação de um elenco brilhante.<strong>Oppenheimer</strong> não é um filme para qualquer um. Ainda que embebido nas famosas assinaturas de Nolan, esse talvez seja o primeiro longa que traz novos aspectos às produções do diretor, tem uma duração mais longa do que o comum e exige concentração do início ao fim.",
+        "photoCover":"https://i0.wp.com/www.arrobanerd.com.br/wp-content/uploads/2023/07/oppenheimer-critica-.jpg?fit=1367%2C769&ssl=1"
+        },
+
+        {
+          "id":"4",
+          "title": "Som da Liberdade",
+          "description": "O polêmico Som da Liberdade, do diretor Alejandro Monteverde, entrou para a lista dos melhores filmes de 2023 até o momento, segundo a lista do IMDb.Protagonizado por Jim Caviezel (Jesus Cristo em <strong>A Paixão de Cristo</strong>), o filme estreou nos cinemas brasileiros recentemente. Nos EUA, logo na estreia, foi apoiado por políticos de direita, como Donald Trump, e bilionários abertamente conservadores, como Elon Musk.No Brasil, tem sido um <strong>trunfo</strong> dos apoiadores de Jair Bolsonaro, que querem torná-lo em uma das maiores bilheterias do ano. O filme conta a história de um esquema de tráfico de crianças na Colômbia que, de acordo com os produtores, é baseado em “fatos reais”.",
+          "photoCover":"https://www.bradojornal.com/midia/news/2023-09-11-som-da-liberdade-primeiro-64fec4e9e46.jpg"
+          },
 
 ]
 
